7e5e4e847856f13228d7349bd6f541ea25d255ac
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-10-30 12:18:06 +0200

    experiment

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,67 +1,60 @@
 #!/usr/bin/python3
-"""
-This file contains the Amenity module
-"""
+"""handles all default RESTFul API actions for Amenity objects"""
+from flask import jsonify, abort, request
 from api.v1.views import app_views
-from flask import jsonify, abort, request, make_response
 from models import storage
 from models.amenity import Amenity
+from api.v1.views.base_actions import REST_actions
 
 
-@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+@app_views.route('/amenities', methods=['GET'])
 def get_all_amenities():
-    """ get amenities by id """
-    all_list = [obj.to_dict() for obj in storage.all(Amenity).values()]
-    return jsonify(all_list)
+    """gets all Amenity objects"""
+    amenities = REST_actions.get(Amenity)
+    return jsonify(amenities)
 
 
-@app_views.route('/amenities/<string:amenity_id>', methods=['GET'],
-                 strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['GET'])
 def get_amenity(amenity_id):
-    """ get amenity by id"""
-    amenity = storage.get(Amenity, amenity_id)
-    if amenity is None:
+    """gets a Amenity object by its id"""
+    amenity = REST_actions.get_by_id(Amenity, amenity_id)
+    if amenity.get('status code') == 404:
         abort(404)
-    return jsonify(amenity.to_dict())
+    return jsonify(amenity.get('object dict'))
 
 
-@app_views.route('/amenities/<string:amenity_id>', methods=['DELETE'],
-                 strict_slashes=False)
-def del_amenity(amenity_id):
-    """ delete amenity by id"""
-    amenity = storage.get(Amenity, amenity_id)
-    if amenity is None:
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
+def delete_amenity(amenity_id):
+    """deletes a Amenity object by its id"""
+    delete_response = REST_actions.delete(Amenity, amenity_id)
+    if delete_response.get('status code') == 404:
         abort(404)
-    amenity.delete()
-    storage.save()
     return jsonify({})
 
 
-@app_views.route('/amenities/', methods=['POST'],
-                 strict_slashes=False)
-def create_obj_amenity():
-    """ create new instance """
-    if not request.get_json():
-        return make_response(jsonify({"error": "Not a JSON"}), 400)
-    if 'name' not in request.get_json():
-        return make_response(jsonify({"error": "Missing name"}), 400)
-    js = request.get_json()
-    obj = Amenity(**js)
-    obj.save()
-    return (jsonify(obj.to_dict()), 201)
+@app_views.route('/amenities', methods=['POST'])
+def post_amenity():
+    """creates a Amenity"""
+    request_body = request.get_json()
+    if not request_body:
+        return jsonify({'error': 'Not a JSON'}), 400
+    if not request_body.get('name'):
+        return jsonify({'error': 'Missing name'}), 400
+    new_amenity = Amenity(name=request_body.get('name'))
+    post_response = REST_actions.post(new_amenity)
+    return post_response.get('object dict'), post_response.get('status code')
 
 
-@app_views.route('/amenities/<string:amenity_id>', methods=['PUT'],
-                 strict_slashes=False)
-def post_amenity(amenity_id):
-    """  """
-    if not request.get_json():
-        return make_response(jsonify({"error": "Not a JSON"}), 400)
-    obj = storage.get(Amenity, amenity_id)
-    if obj is None:
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'])
+def put_amenity(amenity_id):
+    """ updates a Amenity object by its id """
+    request_body = request.get_json()
+    if not request_body:
+        abort(400, "Not a JSON")
+
+    args_to_ignore = ['id', 'created_at', 'updated_at']
+    put_response = REST_actions.put(
+        Amenity, amenity_id, args_to_ignore, request_body)
+    if put_response.get('status code') == 404:
         abort(404)
-    for key, value in request.get_json().items():
-        if key not in ['id', 'created_at', 'updated_at']:
-            setattr(obj, key, value)
-    storage.save()
-    return jsonify(obj.to_dict())
+    return put_response.get('object dict'), put_response.get('status code')
