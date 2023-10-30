25f6ff0526fedf98444d8293061c4ee4f7161e86
a-dabour <ah910905@gmail.com>
2023-10-30 05:46:32 +0200

    retrieving num of each types' objs

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """returns a status of api"""
 from api.v1.views import app_views
 from flask import jsonify, request
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
@@ -9,3 +10,21 @@ def status():
     """returns the status of the api"""
     if request.method == 'GET':
         return jsonify({"status": "ok"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """retrieves the num of each objs based on a type"""
+    if request.method == 'GET':
+        stats = {}
+        classes = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in classes.items():
+            stats[value] = storage.count(key)
+        return jsonify(stats)
