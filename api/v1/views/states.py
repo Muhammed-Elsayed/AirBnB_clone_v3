9e027fb028a2da37e853b3c13a7d33885dab06b3
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-10-30 16:19:59 +0200

    .

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,9 +3,8 @@
 Flask route that returns json status response
 """
 from api.v1.views import app_views
-from flask import abort, jsonify, make_response, request.
+from flask import abort, jsonify, make_response, request
 from models import storage
-from models.db_storage import classes
 
 
 @app_views.route('/states', methods=['GET', 'POST'])
