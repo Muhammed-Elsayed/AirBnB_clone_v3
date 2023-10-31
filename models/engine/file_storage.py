c35d644d09a03dbbeb63b55ead73b6f5d2b4d12a
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-10-31 11:33:04 +0200

    storages fixed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,13 +48,6 @@ class FileStorage:
         with open(self.__file_path, 'w') as f:
             json.dump(json_objects, f)
 
-    def get1(self, cls, id):
-        """retrieves one object of a given class"""
-        if cls and id:
-            obj = f"{cls}.{id}"
-            all_objs = self.all(cls)
-            return all_objs.get(obj)
-        return None
     
     def get(self, cls, id):
         """
