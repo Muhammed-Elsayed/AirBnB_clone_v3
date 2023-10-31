6916882c4b9fd935907348838717e0ee079a7371
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2023-10-31 11:48:17 +0200

    .

--- models/user.py
+++ models/user.py
@@ -28,7 +28,7 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
-    
+
     def __setattr__(self, k, v):
         """sets user pasword"""
         if k == "password":
